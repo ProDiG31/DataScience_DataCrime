3994ecba66c7ffb6c353db97ec77875d92187425
Unknown <rducros34@gmail.com>
2018-10-26 10:25:56 +0200

    Fixing Tuple Premise Key

--- script/databaseDeploy.py
+++ script/databaseDeploy.py
@@ -28,7 +28,7 @@ def useDb(cursor):
 
 def clearDb(cursor):
 	print ("[INFO] - Dropping Previous Tables")
-	cursor.execute("DROP TABLE IF EXISTS %s;" % table_import_temp)
+	# cursor.execute("DROP TABLE IF EXISTS %s;" % table_import_temp)
 	cursor.execute("DROP TABLE IF EXISTS crime;")
 	cursor.execute("DROP TABLE IF EXISTS area;")
 	cursor.execute("DROP TABLE IF EXISTS weapon_type;")
@@ -324,7 +324,10 @@ def extractPremise(cursor):
 	cursor.execute("""
 			  INSERT INTO premise_type(id_premise,premise_description)
 			  SELECT Distinct premise_code,premise_description
-				  FROM t_crime_import;
+				FROM t_crime_import
+				where premise_description != ''
+				group by premise_code
+				order by premise_code;
 			""")
 
 def extractArea(cursor) :
@@ -422,13 +425,13 @@ def deployDatabase(database):
 	conn.commit()
 
 	# ---- Read Cleared data imported from US Open Data
-	# filename = '../source/CrimeDataLight.csv'
+	filename = '../source/CrimeDataLight.csv'
 	# filename = '../source/CrimeDataLight2.csv'
-	filename = '../source/Crime_Data_from_2010_to_Present.csv'
+	# filename = '../source/Crime_Data_from_2010_to_Present.csv'
 	print ('[INFO] - Counting Row ')
 	totalrows =  len(open(filename).readlines()) - 1
-
 	t0 = time.time()
+
 	with open(filename) as csvfile:
 	# ---- Read each line of csv file
 		reader = csv.DictReader(csvfile, delimiter=',' , lineterminator ='\n')
@@ -496,10 +499,10 @@ def deployDatabase(database):
 				# print (query)
 				cursor.execute(query)
 				query = insert
-
+				conn.commit()
 
 	print('Elapsed time : %.2f sec' % (time.time() - t0))
-	conn.commit()
+
 	# ---- create normalized tables
 	initializeNormalizedTable(cursor)
 
