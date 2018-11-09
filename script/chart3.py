9dfa0e7616d7a115396cb7616497e7dfeb1ae192
Leidanium <alexsam.quentin@gmail.com>
2018-11-09 17:45:19 +0100

    budget vs crime

---
+++ script/chart3.py
@@ -0,0 +1,81 @@
+import plotly.plotly as py
+import plotly.figure_factory as ff
+import numpy as np
+import plotly.graph_objs as go
+
+# importing database class
+from databaseUtil import Database
+
+def drawChart(cursor):
+    print ("[INFO] - Requesting data for Chart3")
+
+
+    # print ("[INFO] - Data Received ")
+    crime_x = []
+    budget_x = []
+    data_crime_number = []
+    data_budget_number = []
+
+    cursor.execute("""
+        SELECT YEAR(date_occurred) as years, count(id_crime) as counter FROM `crime`
+        GROUP By years
+        ORDER BY years
+    """)
+
+    for (years, counter ) in cursor:
+        crime_x.append(years)
+        data_crime_number.append(counter)
+
+    cursor.execute("""
+        SELECT `year_budget` as years, `police_budget` as counter FROM `police_budgets`
+    """)
+
+    for (years, counter ) in cursor:
+        budget_x.append(years)
+        data_budget_number.append(counter)
+
+
+    trace1 = go.Scatter(
+        x=crime_x,
+        y=data_crime_number,
+        fill='tozeroy'
+    )
+    trace2 = go.Scatter(
+        x=budget_x,
+        y=data_budget_number,
+        fill='tonexty'
+        yaxis='y2'
+    )
+
+    layout = go.Layout(
+        title='Budget vs Crime',
+        yaxis=dict(
+            title='crimes'
+        ),
+        yaxis2=dict(
+            title='budget',
+            titlefont=dict(
+                color='rgb(148, 103, 189)'
+            ),
+            tickfont=dict(
+                color='rgb(148, 103, 189)'
+            ),
+            overlaying='y',
+            side='right'
+        )
+    )
+
+    data = [trace1, trace2]
+    fig = go.Figure(data=data, layout=layout)
+    py.plot(fig, filename='basic-area', auto_open=True)
+
+
+def deployChart(database):
+    # ---- Connection to Mysql Server
+    conn = database
+
+    # ---- Init Mysql Cursor
+    cursor = conn.cursor
+
+    # ---- drawing first chart
+    drawChart(cursor)
