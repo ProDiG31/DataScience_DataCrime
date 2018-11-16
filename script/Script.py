35b947b13d8b36958193cc1a8b55d119e8d3e7be
Leidanium <alexsam.quentin@gmail.com>
2018-11-16 15:58:33 +0100

    add other chart to script, and commented

--- script/Script.py
+++ script/Script.py
@@ -19,8 +19,14 @@ import plotlySetting
 # from chart2 import deployChart
 # deployChart(db)
 
+# from chart3 import deployChart
+# deployChart(db)
+
+# from chart4 import deployChart
+# deployChart(db)
+
 # from map1 import deployMap
 # deployMap(db)
 
-from chartPie import deployChart
-deployChart(db)
+# from chartPie import deployChart
+# deployChart(db)
