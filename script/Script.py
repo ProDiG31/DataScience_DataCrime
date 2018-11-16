e9883d7cd83c6150d750b6309498dacd723ff9fc
Unknown <rducros34@gmail.com>
2018-11-16 15:11:47 +0100

    Fixing for merge

--- script/Script.py
+++ script/Script.py
@@ -13,10 +13,16 @@ else: print("[INFO] - Deploy skipped")
 
 import plotlySetting
 
-from chart import deployChart
-deployChart(db)
+# from chart import deployChart
+# deployChart(db)
 
-from chart2 import deployChart
+# from chart2 import deployChart
+# deployChart(db)
+
+# from map1 import deployMap
+# deployMap(db)
+
+from chartPie import deployChart
 deployChart(db)
 
 from chart3 import deployChart
